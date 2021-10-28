2990124054eeae43db424bb2a062c5c7f3093f56
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-28 20:09:07 +0200

    svolto esercizio avanzato

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
-// function getNumbers() {
-//     let numbersChoice = prompt("Insert your numbers separated from comma");
-//     const numbers = numbersChoice.split(",");
-//     const parsedNumbers = [];
-//     for (x of numbers) {
-//         parsedNumbers.push(parseInt(x))
-//     };
-//     setTimeout(() => console.log(...parsedNumbers), 10000);
-// }
-// getNumbers()
+let numbersChoice = prompt("Insert your numbers separated from comma");
+const numbers = numbersChoice.split(",");
+const parsedNumbers = [];
+for (x of numbers) {
+    parsedNumbers.push(parseInt(x))
+};
+
+function getNumbers() {
+        setTimeout(() => console.log(...parsedNumbers), 1000);
+}
+getNumbers()
+
+function sumNum() {
+    let sum = 0;
+    for (let i = 0; i < parsedNumbers.length; i++){
+        sum += parsedNumbers[i];
+    }
+    console.log(sum);
+}
+sumNum()
 
 function changeColor() {
     const wrapper = document.querySelector('.wrapper');
@@ -20,10 +30,11 @@ function changeColor() {
       wrapper.classList.toggle(!wrapper.classList.contains("color1"));
     }, 5000);
   }
+
 let intColor = 0;
 
-
 document.body.addEventListener("load", changeColor());
+
 document.addEventListener("keypress", (Event) => {
     if( Event.key === "Enter") {
         clearInterval(intColor);
