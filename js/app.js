3786c85ab64326754ef4f28e7d75188293ce9f00
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-28 17:51:31 +0200

    task 1 & task 2 complete

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function getNumbers() {
+    let numbersChoice = prompt("Insert your numbers separated from comma");
+    const numbers = numbersChoice.split(",");
+    const parsedNumbers = [];
+    for (x of numbers) {
+        parsedNumbers.push(parseInt(x))
+    };
+    setTimeout(() => console.log(...parsedNumbers), 10000);
+}
+getNumbers()
+
+
+window.addEventListener("load", function() {
+    const wrapper = document.querySelector('.wrapper');
+    let counter = 1;
+    setInterval(function() {
+      wrapper.classList.remove("color" + counter);
+      counter++;
+      if (counter > 3) counter = 1;
+      wrapper.classList.add("color" + counter);
+      wrapper.classList.toggle(!wrapper.classList.contains("color1"));
+    }, 1000);
+  });
+
+const stopColorChange = () => {
+    window.removeEventListener("keypress", "Enter");
+}
+
+
+
+
