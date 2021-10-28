6a344ce68411c54af9800e6f1b91a05a93bd760b
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-28 19:38:01 +0200

    ora il cerchio si ferma premendo enter

--- js/app.js
+++ js/app.js
@@ -1,29 +1,34 @@
-function getNumbers() {
-    let numbersChoice = prompt("Insert your numbers separated from comma");
-    const numbers = numbersChoice.split(",");
-    const parsedNumbers = [];
-    for (x of numbers) {
-        parsedNumbers.push(parseInt(x))
-    };
-    setTimeout(() => console.log(...parsedNumbers), 10000);
-}
-getNumbers()
+// function getNumbers() {
+//     let numbersChoice = prompt("Insert your numbers separated from comma");
+//     const numbers = numbersChoice.split(",");
+//     const parsedNumbers = [];
+//     for (x of numbers) {
+//         parsedNumbers.push(parseInt(x))
+//     };
+//     setTimeout(() => console.log(...parsedNumbers), 10000);
+// }
+// getNumbers()
 
 function changeColor() {
     const wrapper = document.querySelector('.wrapper');
     let counter = 1;
-    setInterval(function() {
+    intColor = setInterval(function() {
       wrapper.classList.remove("color" + counter);
       counter++;
       if (counter > 3) counter = 1;
       wrapper.classList.add("color" + counter);
       wrapper.classList.toggle(!wrapper.classList.contains("color1"));
-    }, 1000);
+    }, 5000);
   }
+let intColor = 0;
+
 
 document.body.addEventListener("load", changeColor());
-
-//document.body.removeEventListener("keypress", changeColor)
+document.addEventListener("keypress", (Event) => {
+    if( Event.key === "Enter") {
+        clearInterval(intColor);
+    }
+})
 
 
 
